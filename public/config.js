d2d2e51e1239e114bd22feecce1dd49d7a01e2ec
chuyua <979093525@qq.com>
2022-03-29 17:07:21 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -10,16 +10,25 @@ window.Config = {
   // UptimeRobot Api 域名
   // 只需填写域名部分，默认为官网域名
   // 因官网 API 时不时的会 CROS 报错，可填自定义反代域名
-  // 详见 https://github.com/yb/uptime-status/ 说明
-  ApiDomain: 'api.uptimerobot.com',
+  // 详见说明
+  ApiDomain: 'uptime-status.qikaile.workers.dev',
 
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only 两只 Api Key
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'm791201429-b502ee0caf9a8900bbf58d48',
+    /*
+    'm786322663-402cd609baf66832d847ef63',
+    'm786322674-0773b2c046e85a9cd3344aa8',
+    'm786322754-8bd9c8c0e7f6bc59ddc376e7',
+    'm786322758-45dc64ec941e47673f2f5bd1',
+    'm786336367-8105e54df5e10b1654fe5df7',
+    'm786336382-19b8bc68931082ae50ad13fd',
+    'm786344761-38ac7c80cfba3bff6b4ee9b0',
+    'm786442374-d8ecbfbf82ad151ba3cf94f6',
+    'm787511928-e4ce8aad127e7c28d714985b',
+    'm788502517-d91440d32f8ab5200e337433',
+    */
   ],
 
   // 是否显示监测站点的链接
@@ -34,11 +43,7 @@ window.Config = {
   Navi: [
     {
       text: 'Homepage',
-      url: 'https://status.org.cn/'
+      url: 'https://kamiui.ml/'
     },
-    {
-      text: 'GitHub',
-      url: 'https://github.com/yb/uptime-status'
-    }
   ]
 };
